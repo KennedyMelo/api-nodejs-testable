00048b8cafffd77814f53bedb57496515325e899
KennedyMelo <kennedycmelo@gmail.com>
2023-08-10 01:42:13 -0300

    test: add get Products unit test error

--- test/unit/controllers/products_spec.js
+++ test/unit/controllers/products_spec.js
@@ -1,5 +1,6 @@
 import ProductsController from '../../../src/controllers/products.js';
 import sinon from 'sinon';
+import Product from '../../../src/models/products.js';
 
 describe('Controllers: Products', () => {
   const defaultProduct = [{
@@ -9,17 +10,37 @@ describe('Controllers: Products', () => {
   }];
 
   describe('get() products', () => {
-    it('should return a list of products', () => {
+    it('should return a list of products', async () => {
       const request = {};
       const response = {
         send: sinon.spy()
       };
+      Product.find = sinon.stub();
+      Product.find.withArgs({}).resolves(defaultProduct);
 
-      const productsController = new ProductsController();
-      productsController.get(request, response);
+      const productsController = new ProductsController(Product);
 
-      expect(response.send.called).to.be.true;
-      expect(response.send.calledWith(defaultProduct)).to.be.true;
+      await productsController.get(request, response);
+
+      sinon.assert.calledWith(response.send, defaultProduct);
+    });
+
+    it('should return 400 when an error occurs', async () => {
+      const request = {};
+      const response = {
+        send: sinon.spy(),
+        status: sinon.stub()
+      };
+
+      response.status.withArgs(400).returns(response);
+      Product.find = sinon.stub();
+      Product.find.withArgs({}).rejects({message: 'Error'});
+
+      const productsController = new ProductsController(Product);
+      
+      await productsController.get(request, response);
+
+      sinon.assert.calledWith(response.send, 'Error');
     });
   });
 });
